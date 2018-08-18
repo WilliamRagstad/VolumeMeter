c1b3a91066fbe4048b001ad8b8c19b984d61aadd
yschgroup <32958487+YSCHGroup@users.noreply.github.com>
2018-08-18 16:23:26 +0200

    Initial Publish

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.frequencyVisualizer = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.frequencyVisualizer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // frequencyVisualizer
+            // 
+            this.frequencyVisualizer.Location = new System.Drawing.Point(51, 13);
+            this.frequencyVisualizer.Name = "frequencyVisualizer";
+            this.frequencyVisualizer.Size = new System.Drawing.Size(737, 179);
+            this.frequencyVisualizer.TabIndex = 0;
+            this.frequencyVisualizer.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Master";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Left";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Right";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(612, 198);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = -100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(577, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Slope";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(722, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Animate";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(773, 202);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 220);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.frequencyVisualizer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Volume Visualizer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.frequencyVisualizer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox frequencyVisualizer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public bool listen = true;
+        public Thread LTS;
+        public SolidBrush Color_background = new System.Drawing.SolidBrush(Color.GhostWhite);
+        public Pen Color_bar = new System.Drawing.Pen(Color.FromArgb(80, 240, 123));
+        public Pen Color_bar_drop = new System.Drawing.Pen(Color.FromArgb(0, 160, 103));
+        public float Bar_drop_size = 10;
+        public float Bar_length_inc = 1f;
+        public float Bar_length_scale = 0f;
+        public float Time_Tickrate = 1 / 10f;
+
+        public float time;
+        public bool Animate = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            time = 0;
+
+            LTS = new Thread(ListenToSterio);
+            LTS.Start();
+        }
+
+        private void ListenToSterio()
+        {
+            MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            while (true)
+            {
+                if (Animate)
+                {
+                    time += Time_Tickrate;
+                    Bar_length_inc = (float) Math.Sin(time) * Bar_length_scale;
+                }
+                else
+                {
+                    Bar_length_inc = Bar_length_scale;
+                }
+
+                if (listen)
+                {
+                    var m = defaultDevice.AudioMeterInformation.MasterPeakValue;    // Master
+                    var l = defaultDevice.AudioMeterInformation.PeakValues[0];      // left
+                    var r = defaultDevice.AudioMeterInformation.PeakValues[1];      // right
+
+                    if (!m.Equals(0))
+                    {
+                        // Send information to the form
+                        frequencyVisualizer.Image = CreateBars(m, l, r);
+                        Thread.Sleep(40);
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LTS.Abort();
+            listen = false;
+        }
+
+        private Bitmap CreateBars(float master, float left, float right)
+        {
+            Bitmap img = new Bitmap(frequencyVisualizer.Width, frequencyVisualizer.Height);
+            Graphics mockup = Graphics.FromImage(img);
+
+            //   |####################|
+            //   |#######             |
+            //   |############________|
+
+            mockup.FillRectangle(Color_background, 0, 0, img.Width, img.Height);   // Background
+
+            // Fill a bar for master
+            for (int i = 0; i < img.Height/3; i++)
+            {
+                float length = img.Width * master + i * Bar_length_inc;
+                if (i < img.Height / 3 - Bar_drop_size)
+                {
+                    mockup.DrawLine(Color_bar, 0, i, length, i);
+                }
+                else
+                {
+                    mockup.DrawLine(Color_bar_drop, 0, i, length, i);
+                }
+            }
+
+            // Fill a bar for left speaker
+            for (int j = img.Height/3; j < img.Height/3 * 2; j++)
+            {
+                float length = img.Width * left + j * Bar_length_inc;
+                if (j < img.Height / 3 * 2 - Bar_drop_size)
+                {
+                    mockup.DrawLine(Color_bar, 0, j, length, j);
+                }
+                else
+                {
+                    mockup.DrawLine(Color_bar_drop, 0, j, length, j);
+                }
+            }
+
+            // Fill a bar for right speaker
+            for (int k = img.Height/3 * 2; k < img.Height/3 * 3; k++)
+            {
+                float length = img.Width * right + k * Bar_length_inc;
+                if (k < img.Height / 3 * 3 - Bar_drop_size)
+                {
+                    mockup.DrawLine(Color_bar, 0, k, length, k);
+                }
+                else
+                {
+                    mockup.DrawLine(Color_bar_drop, 0, k, length, k);
+                }
+            }
+
+            return img;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Bar_length_scale = (float)trackBar1.Value / 100;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Animate = checkBox1.Checked;
+        }
+    }
+}
